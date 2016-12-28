756d4cba9f058ebd474b5deec36fc11babdcb713
muhamadfahrulazis <fahrulazis287@gmail.com>
2016-12-28 16:38:20 +0700

    Add files via upload

---
+++ KostLine1/application/views/layout/header.php
@@ -0,0 +1,22 @@
+<div class="container">
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="sr-only">Toggle Responsive</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="#" class="navbar-brand">KostLine.com</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="<?php echo base_url(); ?>daftar">Daftar</a></li>
+                    <li><a href="<?php echo base_url(); ?>login">Masuk</a></li>
+                    <li><a href="">Bantuan</a></li>
+                    <li><input type="text" placeholder="Cari Kost" class="form-control"></li>
+                    <li><input class="btn btn-success" type="submit" value="Search"></li>
+                </ul>
+            </div>
+        </nav>  	
+    </div>
